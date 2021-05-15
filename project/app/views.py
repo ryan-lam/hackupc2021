20b22b52e865c0bd4a8a605931026d7d5695b204
ryan-lam <ryan-lam@users.noreply.github.com>
2021-05-15 01:13:43 -0400

    added backend for index/login page and signup page

--- project/app/views.py
+++ project/app/views.py
@@ -4,7 +4,62 @@ from django import forms
 from django.urls import reverse
 from .models import Profile, Job, Housing
 
-# Create your views here.
 
+# INDEX PAGE
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        if Profile.objects.filter(username=username, password=password).exists():
+            request.session["username"] = username
+            return HttpResponseRedirect(reverse("dashboard"))
+        else:
+            return render (request, "index.html", {
+                "attempt":True
+                })
+    
+    else: # GET METHOD
+        request.session.flush()
+        return render(request, "index.html", {
+            "attempt":False
+        })
+
+
+# SIGNUP PAGE
+def signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        name = request.POST["name"]
+        age = request.POST["age"]
+        gender = request.POST["gender"]
+        uni_name = request.POST["uni_name"]
+        major = request.POST["major"]
+        gpa = request.POST["gpa"]
+
+        try:
+            profile = Profile(name=name, age=age, gender=gender, major=major, uni_name=uni_name, email=email, password=password, gpa=gpa)
+            profile.save()
+            request.session["username"] = username
+            return HttpResponseRedirect(reverse("dashboard"))
+        except:
+            return render(request, "signup.html", {
+                "attempt":True
+            })
+
+    else: # GET METHOD
+        return render(request, "signup.html", {
+            "attempt":False
+        })
+
+
+
+
+
+
+
+
+
+def signup(request):
+    pass
